d056a09f0ce1f571683ff3d90286f09e64bbc195
Sreevani <sreevani.y@everest.engineering>
2026-02-13 15:44:33 +0530

    Modify the config files

--- config/databaseConnection.js
+++ config/databaseConnection.js
@@ -1,17 +1,14 @@
-const { Sequelize } = require('sequelize');
+import { Sequelize } from "sequelize";
+import * as dbConfig from "./config/config.json";
+
+const env = process.env.NODE_ENV || "development";
+const config = dbConfig[env];
 
 const sequelize = new Sequelize(
-  process.env.DATABASE,
-  process.env.DATABASE_USERNAME,
-  process.env.DATABASE_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: process.env.DATABASE_DIALECT,
-    port: process.env.DB_PORT || 5432,
-    dialectOptions: process.env.DATABASE_DIALECT === 'postgres' ? {
-      ssl: { require: true, rejectUnauthorized: false }
-    } : {}
-  }
+  config.database,
+  config.username,
+  config.password,
+  config
 );
 
-module.exports = sequelize;
+export default sequelize;
